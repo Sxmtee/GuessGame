b3bfb7e1aa787fb44adf489d73e5a68000565b1b
sxmtee <somtoo76@gmail.com>
2024-04-02 00:08:37 +0100

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by mac on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

61f14d313be5adf152dbbd190a93f116090fff94
Somtochukwu <somtoo76@gmail.com>
2024-11-16 21:00:05 +0100

    Added accessibility and voice control

--- GuessGame/TheGame.swift
+++ GuessGame/TheGame.swift
@@ -35,6 +35,20 @@ struct TheGame: View {
         "Ukraine",
         "US"
     ].shuffled()
+
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -127,6 +141,7 @@ struct TheGame: View {
                         } label: {
                             FlagImage(imageName: countries[number])
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         .rotation3DEffect(
                             .degrees(number == selectedFlag ? animationAmount : 0),
                             axis: (x: 0, y: 1, z: 0))
